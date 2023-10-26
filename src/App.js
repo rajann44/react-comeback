cfd715540c17e302d73d182ce88c221267950154
Rajan Chaudhary <rajann44@gmail.com>
2023-10-27 01:49:26 +0200

    first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-success">Welcome to React Comeback Winter 2023</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
