2f1c8bfb037fa432ee58f2e7f7c20c83a79562fc
Rajan Chaudhary <rajann44@gmail.com>
2023-10-27 18:54:48 +0200

    form and events

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Email: ${formData.email}, Password: ${formData.password}`);
+  };
+
+  return (
+    <form className="container-sm my-3" onSubmit={handleSubmit}>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter email in correct format using @"
+          value={formData.email}
+          onChange={(event) =>
+            setFormData({ ...formData, email: event.target.value })
+          }
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+          value={formData.password}
+          onChange={(event) =>
+            setFormData({ ...formData, password: event.target.value })
+          }
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default Contact;
