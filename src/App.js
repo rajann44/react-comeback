843fb84d7a027f75b51af94322cb3f9dcd18d7d8
Rajan Chaudhary <rajann44@gmail.com>
2023-10-27 02:06:42 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/home" element={<Homepage></Homepage>} />
+        <Route path="/form" element={<div>Form page</div>} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //Below is basename which will be appended pre url for each route
+  <BrowserRouter basename="/app">
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 );
